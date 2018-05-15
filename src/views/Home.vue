ba3557bd67d74f5fd8b783354ff5141f05300ef0
Oussama Zouaki <oussamazouaki4@gmail.com>
2018-05-15 00:16:18 +0000

    improve auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,52 @@
 <template>
-    <v-container>
-        <v-layout row>
+    <v-container align-content-start>
+        <v-layout row align-content-start>
             <Post class="elevation-1"></Post>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-import axios from 'axios'
 
 import Post from '../components/Post.vue'
+import _user from '../graph/user.query.graphql'
+import graph from '../graph/client'
 
 export default {
     data: () => ({
 
     }),
-    created () {},
+    created () {
+        const token = localStorage.getItem('authToken')
+        graph.query({
+            query: _user,
+            variables: {
+                token: localStorage.getItem('authToken')
+            },
+            context: {
+                headers: {
+                    authorization: token ? `Bearer ${token}` : ''
+                }
+            }
+
+        }).then(res => {
+            console.log('res ', res.data.userByToken)
+            const u = res.data.userByToken
+            this.$store.commit('setUser', {
+                id: u.id,
+                first_name: u.firstName,
+                last_name: u.lastName,
+                email: u.email,
+                avatar: u.avatar,
+                bio: u.bio,
+                date_joined: u.dateJoined
+            })
+        }).catch(err => {
+            console.log('err ', err.networkError.result)
+            this.$store.commit('setUser', null)
+            this.$router.push('/login')
+        })
+    },
     components: {
         Post
     }
